dc7f415ccf934f6a62dfac509764dae6840f67dd
franz paran <fparan@melistechnology.com>
2019-02-18 13:26:03 +0800

    new changes for english translations and added french translations

--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -1,4 +1,7 @@
 <?php
     return [
-
+        'tr_meliscommerce_order_invoice_export_prompt_title' => 'Facture',
+        'tr_meliscommerce_order_invoice_export_no_invoice' => 'Facture indisponible pour cette commande',
+        'tr_meliscommerce_order_invoice_export_pdf_title' => 'Exporter PDF',
+        'tr_meliscommerce_order_invoice_button_regenerate_invoice' => 'Regénérer la facture',
     ];
