66c258920a8abc1135a6198414b299ba0ec50260
fparan <fparan@melistechnology.com>
2019-03-08 18:59:21 +0800

    added translations for french and english

--- language/en_EN.interface.php
+++ language/en_EN.interface.php
@@ -4,4 +4,18 @@
         'tr_meliscommerce_order_invoice_export_no_invoice' => 'Invoice unavailable for this order',
         'tr_meliscommerce_order_invoice_export_pdf_title' => 'Export PDF',
         'tr_meliscommerce_order_invoice_button_regenerate_invoice' => 'Regenerate Invoice',
+        'tr_meliscommerce_order_invoice_button_download_invoice' => 'Download Invoice',
+        // PDF TRANSLATIONS
+        'tr_meliscommerceorderinvoice_pdf_invoice' => 'INVOICE',
+        'tr_meliscommerceorderinvoice_pdf_date' => 'DATE',
+        'tr_meliscommerceorderinvoice_pdf_bill_to' => 'BILL TO',
+        'tr_meliscommerceorderinvoice_pdf_items' => 'ITEMS',
+        'tr_meliscommerceorderinvoice_pdf_price' => 'PRICE',
+        'tr_meliscommerceorderinvoice_pdf_qty' => 'QTY',
+        'tr_meliscommerceorderinvoice_pdf_discount' => 'DISCOUNT',
+        'tr_meliscommerceorderinvoice_pdf_amount' => 'AMOUNT',
+        'tr_meliscommerceorderinvoice_pdf_subtotal' => 'SUB TOTAL',
+        'tr_meliscommerceorderinvoice_pdf_shipping' => 'SHIPPING',
+        'tr_meliscommerceorderinvoice_pdf_total' => 'TOTAL',
+        'tr_meliscommerceorderinvoice_pdf_comments' => 'COMMENTS'
     ];
--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -4,4 +4,18 @@
         'tr_meliscommerce_order_invoice_export_no_invoice' => 'Facture indisponible pour cette commande',
         'tr_meliscommerce_order_invoice_export_pdf_title' => 'Exporter PDF',
         'tr_meliscommerce_order_invoice_button_regenerate_invoice' => 'Regénérer la facture',
+        'tr_meliscommerce_order_invoice_button_download_invoice' => 'Exporter la facture',
+        // PDF TRANSLATIONS
+        'tr_meliscommerceorderinvoice_pdf_invoice' => 'FACTURE',
+        'tr_meliscommerceorderinvoice_pdf_date' => 'DATE',
+        'tr_meliscommerceorderinvoice_pdf_bill_to' => 'FACTURER A',
+        'tr_meliscommerceorderinvoice_pdf_items' => 'PANIER',
+        'tr_meliscommerceorderinvoice_pdf_price' => 'PRIX',
+        'tr_meliscommerceorderinvoice_pdf_qty' => 'QUANTITE',
+        'tr_meliscommerceorderinvoice_pdf_discount' => 'REMISE',
+        'tr_meliscommerceorderinvoice_pdf_amount' => 'TOTAL',
+        'tr_meliscommerceorderinvoice_pdf_subtotal' => 'SOUS TOTAL',
+        'tr_meliscommerceorderinvoice_pdf_shipping' => 'COUT ENVOI',
+        'tr_meliscommerceorderinvoice_pdf_total' => 'TOTAL',
+        'tr_meliscommerceorderinvoice_pdf_comments' => 'COMMENTAIRES'
     ];
